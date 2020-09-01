bcf5ef369016c018f48784a966b13b201c8ba6c9
Michael O'Connell <michaeljoconnell@ufl.edu>
2020-08-31 22:57:02 -0400

    removed build files and added to .gitignore

--- SFML_For_Fun/SFML_For_Fun.cpp
+++ SFML_For_Fun/SFML_For_Fun.cpp
@@ -15,11 +15,38 @@ using namespace std;
 int main()
 {
 	// Initialize Game Objects
+
+	sf::RectangleShape rect(sf::Vector2f(500.0f, 300.0f));
+	rect.setFillColor(sf::Color::Yellow);
+	rect.setPosition(view_size.x / 2, view_size.y / 2);
+	rect.setOrigin(sf::Vector2f(rect.getSize().x / 2,
+		rect.getSize().y / 2));
+
+	sf::CircleShape circle(100);
+	circle.setFillColor(sf::Color::Green);
+	circle.setPosition(view_size.x / 2, view_size.y / 2);
+	circle.setOrigin(sf::Vector2f(circle.getRadius(), circle.getRadius()));
+
+	sf::ConvexShape triangle;
+	triangle.setPointCount(3);
+	triangle.setPoint(0, sf::Vector2f(-100, 0));
+	triangle.setPoint(1, sf::Vector2f(0, -100));
+	triangle.setPoint(2, sf::Vector2f(100, 0));
+	triangle.setFillColor(sf::Color(128, 0, 128, 255));
+	triangle.setPosition(view_size.x / 2, view_size.y / 2);
+
+	
 	while (window.isOpen())
 	{
 		// Handle Keyboard Events
 		// Update Game Objects in the Scene
+
+		window.clear(sf::Color::Red);
 		// Render Game Objects
+		window.draw(rect);
+		window.draw(circle);
+		window.draw(triangle);
+		window.display();
 	}
 	return 0;
 }
