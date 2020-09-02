5ff4fa80167038efdec1e92834ab68f93dcad250
Michael O'Connell <michaeljoconnell@ufl.edu>
2020-09-02 10:04:23 -0400

    Added hero class

--- SFML_For_Fun/SFML_For_Fun.cpp
+++ SFML_For_Fun/SFML_For_Fun.cpp
@@ -2,6 +2,7 @@
 //
 #include "SFML_For_Fun.h"
 #include "SFML/Graphics.hpp"
+#include  "Hero.h"
 
 sf::Vector2f view_size(1024, 768);
 sf::VideoMode video_mode(view_size.x, view_size.y);
@@ -15,13 +16,7 @@ sf::Sprite sky_sprite;
 sf::Texture bg_texture;
 sf::Sprite bg_sprite;
 
-sf::Texture hero_texture;
-sf::Sprite hero_sprite;
-
-sf::Vector2f player_position;
-bool player_moving = false;
-
-using namespace std;
+Hero hero;
 
 void init()
 {
@@ -31,17 +26,9 @@ void init()
 	bg_texture.loadFromFile("Assets/graphics/bg.png");
 	bg_sprite.setTexture(bg_texture);
 
-	hero_texture.loadFromFile("Assets/graphics/hero.png");
-	hero_sprite.setTexture(hero_texture);
-	hero_sprite.setPosition(sf::Vector2f(view_size.x / 2, view_size.y / 2));
-	hero_sprite.setOrigin(hero_texture.getSize().x / 2.0f, hero_texture.getSize().y / 2.0f);
-}
-
-void draw()
-{
-	window.draw(sky_sprite);
-	window.draw(bg_sprite);
-	window.draw(hero_sprite);
+	hero.init("Assets/graphics/hero.png", 
+		sf::Vector2f(view_size.x * 0.25f, 
+			view_size.y * 0.5f), 200);
 }
 
 void updateInput()
@@ -52,16 +39,9 @@ void updateInput()
 	{
 		if (event.type == sf::Event::KeyPressed)
 		{
-			if (event.key.code == sf::Keyboard::Right)
+			if (event.key.code == sf::Keyboard::Up)
 			{
-				player_moving = true;
-			}
-		}
-		if (event.type == sf::Event::KeyReleased)
-		{
-			if (event.key.code == sf::Keyboard::Right)
-			{
-				player_moving = false;
+				hero.jump(750.0f);
 			}
 		}
 		if (event.key.code == sf::Keyboard::Escape || 
@@ -74,10 +54,14 @@ void updateInput()
 
 void update(float dt)
 {
-	if (player_moving)
-	{
-		hero_sprite.move(50.0f * dt, 0);
-	}
+	hero.update(dt);
+}
+
+void draw()
+{
+	window.draw(sky_sprite);
+	window.draw(bg_sprite);
+	window.draw(hero.getSprite());
 }
 
 int main()
